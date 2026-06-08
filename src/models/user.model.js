523a66848fbc23492c2d30e6216b14fa376e41dd
Anupam Anand <theanupamojha999@gmail.com>
2026-06-08 23:04:49 +0530

    add models

---
+++ src/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        require: true,   
+    },
+    email: {
+        type: String,
+        require: true,
+
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel
