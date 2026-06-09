52fb57d068f1f0e77ab7c456b0460daa13ea8ec0
Anupam Anand <theanupamojha999@gmail.com>
2026-06-09 18:27:25 +0530

    register user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,40 @@
 import userModel from "../models/user.model";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 const registerUser = async (req, res) => {
     try{
         const {username, email, password}= req.body;
         console.log("user registered sucessfully")
 
+        const isUserAlreadyExists = await userModel.findOne({
+            $or:[
+                {username},
+                {email}
+            ]
+        })
+
+        if(isUserAlreadyExists){
+            console.log("user already exist")
+           return res.status(401).json({message: "user already exists"})
+        }
+        
+        const hash = await bcrypt.hash(password,10);
+
+        const user = await userModel.create({
+            username,
+            email,
+            password:hash
+        })
+
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+        res.cookie("token", token);
+
+        res.status(201).json({message: "user registered sucessfully"})
+
     }
     catch(err){
-        
+        console.error("user not registered", err)
     }
 }
