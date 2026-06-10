1cccd4c03a1802e05b80d166d9cd88e5c1d40fe6
Anupam Anand <theanupamojha999@gmail.com>
2026-06-10 22:28:38 +0530

    feat: real time connection

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 import'dotenv/config';
+import http from "http";
+import { Server } from "socket.io";
 import app from './src/app';
 import connectDB  from './src/config/db';
 
+
+
 connectDB();
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+import socketHandler from "./src/socket/socket.js";
+
+socketHandler(io);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ socket/socket.js
@@ -0,0 +1,31 @@
+const users = {};
+
+const socketHandler = (io) => {
+  io.on("connection", (socket) => {
+
+    console.log("User Connected", socket.id);
+
+    socket.on("join", (userId) => {
+      users[userId] = socket.id;
+    });
+
+    socket.on("send-message", (data) => {
+
+      const receiverSocketId =
+        users[data.receiverId];
+
+      if (receiverSocketId) {
+        io.to(receiverSocketId).emit(
+          "receive-message",
+          data
+        );
+      }
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User Disconnected");
+    });
+  });
+};
+
+export default socketHandler;
