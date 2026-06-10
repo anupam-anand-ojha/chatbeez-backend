b0a7a1e4bdf53324080c04d521b6d9a920e40d5a
Anupam Anand <theanupamojha999@gmail.com>
2026-06-10 21:43:25 +0530

    feat: login API

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,10 +3,15 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 
-const registerUser = async (req, res) => {
+export const registerUser = async (req, res) => {
     try{
         const {username, email, password}= req.body;
-        console.log("user registered sucessfully")
+        
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "All fields are required"
+            });
+        }
 
         
 
@@ -33,20 +38,69 @@ const registerUser = async (req, res) => {
         const token = jwt.sign({id:user._id}, process.env.JWT_SECRET,{ expiresIn: "7d"});
         res.cookie("token", token);
 
-        res.status(201).json({
-         success: true,
-         token,
-         user: {
-          id: user._id,
-          username: user.username,
-          email: user.email
-    }
-});
+         return res.status(201).json({
+            success: true,
+            message: "User registered successfully"
+        });
+  
 
     }
-    catch(err){
-        console.error("user not registered", err)
+    catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
     }
 }
 
-export default {registerUser}
+
+//user login API
+
+export const loginUser = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.cookie("token", token);
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successful"
+        });
+
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+};
