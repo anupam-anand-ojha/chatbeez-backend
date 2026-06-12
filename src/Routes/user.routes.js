5a57110e88a67eb00fe757d15dccf30cd4465b71
Anupam Anand <theanupamojha999@gmail.com>
2026-06-12 14:51:01 +0530

    update: user API

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
-import { getProfile } from "../controllers/user.controller.js";
+
+import { getProfile,getAllUsers} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/profile", authMiddleware, getProfile);
+router.get("/profile", authMiddleware,getProfile);
+
+router.get("/",authMiddleware,getAllUsers);
 
 export default router;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import userModel from "../models/user.model.js";
 
 export const getProfile = async (req, res) => {
-
     try {
 
         const user = await userModel
@@ -9,6 +8,25 @@ export const getProfile = async (req, res) => {
             .select("-password");
 
         return res.status(200).json(user);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+};
+
+export const getAllUsers = async (req, res) => {
+    try {
+
+        const users = await userModel
+            .find(
+                { _id: { $ne: req.user.id } },
+                "-password"
+            );
+
+        return res.status(200).json(users);
 
     } catch (error) {
 
