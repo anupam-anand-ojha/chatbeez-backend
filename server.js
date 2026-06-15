2ed122985796bdccaa3e3564a6ef3daab2528da8
Anupam Anand <theanupamojha999@gmail.com>
2026-06-15 21:07:51 +0530

    fix server

--- server.js
+++ server.js
@@ -23,7 +23,5 @@ socketHandler(io);
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
