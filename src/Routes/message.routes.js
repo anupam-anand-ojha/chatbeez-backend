96c03a801d62c52f1f83eda3d48cbafd69aba326
Anupam Anand <theanupamojha999@gmail.com>
2026-06-15 19:42:03 +0530

    fix routes

--- src/Routes/message.routes.js
+++ src/Routes/message.routes.js
@@ -1,23 +1,12 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 
-import {
-  sendMessage,
-  getMessages,
-} from "../controllers/message.controller.js";
+import {sendMessage,getMessages,} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.post(
-  "/send",
-  authMiddleware,
-  sendMessage
-);
+router.post("/send",authMiddleware,sendMessage);
 
-router.get(
-  "/:userId",
-  authMiddleware,
-  getMessages
-);
+router.get("/:userId", authMiddleware,getMessages);
 
 export default router;
