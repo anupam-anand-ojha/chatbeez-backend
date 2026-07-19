42fa64ae23dfec2f1ce95315521c7a8f2a94c83b
Anupam Anand <theanupamojha999@gmail.com>
2026-07-19 17:34:31 +0530

    add mark as seen routes

--- src/Routes/message.routes.js
+++ src/Routes/message.routes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
 
-import {sendMessage,getMessages,} from "../controllers/message.controller.js";
+import {sendMessage,getMessages,markAsSeen} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.post("/send",authMiddleware,sendMessage);
 
 router.get("/:userId", authMiddleware,getMessages);
+
+router.put("/messages/seen/:userId",markAsSeen)
 
 export default router;
